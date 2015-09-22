49b97799414dec2844b3f6319a1d78091162f03a
Ah-01 <shivam@ahex.com>
2015-09-22 12:09:11 +0530

    User input styling column added

--- js/barChartLib.js
+++ js/barChartLib.js
@@ -8,28 +8,28 @@
  * @author    
  */
 
-function getDataAndStyles(){
+function getDataAndStyles() {
     var chartDataAssigned = $.Deferred();
     var chartStyleAssigned = $.Deferred();
-    var parsedData,parsedStyle;
-   $.post( "json/data.json", function(data) {
-            parsedData = data;
-            chartDataAssigned.resolve();
-});
-   $.post( "json/style.json", function(data) {
-            parsedStyle = data;
-            chartStyleAssigned.resolve();
-});
+    var parsedData, parsedStyle;
+    $.post("json/data.json", function (data) {
+        parsedData = data;
+        chartDataAssigned.resolve();
+    });
+    $.post("json/style.json", function (data) {
+        parsedStyle = data;
+        chartStyleAssigned.resolve();
+    });
 
-var necessaryDataAssigned = $.when(chartDataAssigned, chartStyleAssigned).done(function() {
-     chartInitParams = {
-        Data:parsedData,
-        Style:parsedStyle
-        
-    }
-var graphHandle = new BarChart(chartInitParams);
-graphHandle.render();
-})
+    var necessaryDataAssigned = $.when(chartDataAssigned, chartStyleAssigned).done(function () {
+        chartInitParams = {
+            Data: parsedData,
+            Style: parsedStyle
+
+        }
+        var graphHandle = new BarChart(chartInitParams);
+        graphHandle.render();
+    })
 }
 
 getDataAndStyles();
@@ -45,58 +45,58 @@ getDataAndStyles();
  * @author    
  */
 
-function BarChart(chartInitParams){
-  this.parsedData = chartInitParams.Data;
-  this.parsedStyle = chartInitParams.Style;
-  this.padding ={left:80,right:40,top:20,bottom:30}
+function BarChart(chartInitParams) {
+    this.parsedData = chartInitParams.Data;
+    this.parsedStyle = chartInitParams.Style;
+    this.padding = {left: 80, right: 40, top: 20, bottom: 30}
 
 
     //Defining styles for value axis
-  var valueAxisStyle = this.parsedStyle.Axis.valueAxis;
-  this.valueAxisColor = valueAxisStyle.color;
-  this.valueAxisFontSize = valueAxisStyle.fontSize;
-  this.valueAxisShowAxis = valueAxisStyle.showAxis;
-  this.valueAxisStrokeColor = valueAxisStyle.strokeColor;
-  this.valueAxisStrokeWeight = valueAxisStyle.weight;
-  
-  //Defining styles for category axis
-  var categoryAxisStyle = this.parsedStyle.Axis.categoryAxis;
-  this.categoryAxisColor = categoryAxisStyle.color;
-  this.categoryAxisFontSize = categoryAxisStyle.fontSize;
-  this.categoryAxisShowAxis = categoryAxisStyle.showAxis;
-  this.categoryAxisStrokeColor = categoryAxisStyle.strokeColor;
-  this.categoryAxisStrokeWeight = categoryAxisStyle.weight;
-  
-  //Defining seriesData Styles
-  this.seriesData = this.parsedStyle.SeriesStyles;
-  
-  //Defining styles for charting Area
-  var chartingAreaStyle = this.parsedStyle.chartingAreaStyle;
-  this.chartingAreaBackgroundColor = chartingAreaStyle.backgroundColor;
-  this.chartingAreaBorderColor = chartingAreaStyle.borderColor;
-  this.chartingAreaBorderThickness = chartingAreaStyle.borderThickness;
-  this.chartingAreaXPosition = chartingAreaStyle.chartComponentPositionX;
-  this.chartingAreaYPosition = chartingAreaStyle.chartComponentPositionY;
-  this.chartingAreaIsVisible = chartingAreaStyle.chartComponentIsVisible;
-  this.chartingAreaHeight = chartingAreaStyle.chartComponentHeight;
-  this.chartingAreaWidth = chartingAreaStyle.chartComponentWidth;
-  
-  //Defining grid line style
-  var gridLineStyle = this.parsedStyle.gridStyle;
-  this.horizontalLineStrokeColor = gridLineStyle.hLineStrokecolor;
-  this.horizontalLineThickness = gridLineStyle.hLineThickness;
-  this.IsGridLineVisible = gridLineStyle.isGridlineVisible;
-  this.verticalLineStrokeColor = gridLineStyle.vLineStrokeColor;
-  this.verticalLineThickness = gridLineStyle.vLineThickness;
-  
-  //Defining legend styles
-  var legendStyles = this.parsedStyle.legendStyles;
-  this.chartLegendFontSize = legendStyles.fontSize;
-  this.chartLegendFontFamily = legendStyles.fontFamily;
-  this.chartLegendFontStyle = legendStyles.fontStyle;
-  this.chartLegendFontWeight = legendStyles.fontWeight;
-  
-  this.spliceData();
+    var valueAxisStyle = this.parsedStyle.Axis.valueAxis;
+    this.valueAxisColor = valueAxisStyle.color;
+    this.valueAxisFontSize = valueAxisStyle.fontSize;
+    this.valueAxisShowAxis = valueAxisStyle.showAxis;
+    this.valueAxisStrokeColor = valueAxisStyle.strokeColor;
+    this.valueAxisStrokeWeight = valueAxisStyle.weight;
+
+    //Defining styles for category axis
+    var categoryAxisStyle = this.parsedStyle.Axis.categoryAxis;
+    this.categoryAxisColor = categoryAxisStyle.color;
+    this.categoryAxisFontSize = categoryAxisStyle.fontSize;
+    this.categoryAxisShowAxis = categoryAxisStyle.showAxis;
+    this.categoryAxisStrokeColor = categoryAxisStyle.strokeColor;
+    this.categoryAxisStrokeWeight = categoryAxisStyle.weight;
+
+    //Defining seriesData Styles
+    this.seriesData = this.parsedStyle.SeriesStyles;
+
+    //Defining styles for charting Area
+    var chartingAreaStyle = this.parsedStyle.chartingAreaStyle;
+    this.chartingAreaBackgroundColor = chartingAreaStyle.backgroundColor;
+    this.chartingAreaBorderColor = chartingAreaStyle.borderColor;
+    this.chartingAreaBorderThickness = chartingAreaStyle.borderThickness;
+    this.chartingAreaXPosition = chartingAreaStyle.chartComponentPositionX;
+    this.chartingAreaYPosition = chartingAreaStyle.chartComponentPositionY;
+    this.chartingAreaIsVisible = chartingAreaStyle.chartComponentIsVisible;
+    this.chartingAreaHeight = chartingAreaStyle.chartComponentHeight;
+    this.chartingAreaWidth = chartingAreaStyle.chartComponentWidth;
+
+    //Defining grid line style
+    var gridLineStyle = this.parsedStyle.gridStyle;
+    this.horizontalLineStrokeColor = gridLineStyle.hLineStrokecolor;
+    this.horizontalLineThickness = gridLineStyle.hLineThickness;
+    this.IsGridLineVisible = gridLineStyle.isGridlineVisible;
+    this.verticalLineStrokeColor = gridLineStyle.vLineStrokeColor;
+    this.verticalLineThickness = gridLineStyle.vLineThickness;
+
+    //Defining legend styles
+    var legendStyles = this.parsedStyle.legendStyles;
+    this.chartLegendFontSize = legendStyles.fontSize;
+    this.chartLegendFontFamily = legendStyles.fontFamily;
+    this.chartLegendFontStyle = legendStyles.fontStyle;
+    this.chartLegendFontWeight = legendStyles.fontWeight;
+
+    this.spliceData();
 }
 
 /*  -------------------------------------------------------------------------   */
@@ -108,12 +108,12 @@ function BarChart(chartInitParams){
  * @access    public
  * @author    
  */
-BarChart.prototype.spliceData = function() {
+BarChart.prototype.spliceData = function () {
     var that = this;
-     
-     that.finalGraphData = this.parsedData;
-     
-         /*  defining the scale for x-axis  */
+
+    that.finalGraphData = this.parsedData;
+
+    /*  defining the scale for x-axis  */
     that.xScale = d3.scale.linear();
 
     /*  defining the scale for value axis */
@@ -121,40 +121,40 @@ BarChart.prototype.spliceData = function() {
     that.barScale = d3.scale.ordinal();
 
     var valueArray = [];
- $.each(that.finalGraphData.Data,function(key,value){
-    $.each(value.data,function(k,v){
-           valueArray.push(v.value);
-    })
-});
- that.valueAxisMinVal = d3.min(valueArray);
- that.valueAxisMaxVal = d3.max(valueArray);
+    $.each(that.finalGraphData.Data, function (key, value) {
+        $.each(value.data, function (k, v) {
+            valueArray.push(v.value);
+        })
+    });
+    that.valueAxisMinVal = d3.min(valueArray);
+    that.valueAxisMaxVal = d3.max(valueArray);
 
-if(that.valueAxisMinVal > 0){
-    that.valueAxisMinVal = 0;
-}
+    if (that.valueAxisMinVal > 0) {
+        that.valueAxisMinVal = 0;
+    }
 
-that.actualChartingAreaWidth = that.chartingAreaWidth;
-that.actualChartingAreaHeight = that.chartingAreaHeight;
+    that.actualChartingAreaWidth = that.chartingAreaWidth;
+    that.actualChartingAreaHeight = that.chartingAreaHeight;
 
-   /*  calculate the svg height and width  */
+    /*  calculate the svg height and width  */
     that.width = that.actualChartingAreaWidth - that.padding.left - that.padding.right;
     that.height = that.actualChartingAreaHeight - that.padding.top - that.padding.bottom;
- /*  defining the scale for x-axis  */
+    /*  defining the scale for x-axis  */
     that.xScale = d3.scale.linear();
 
     /*  defining the scale for value axis */
     that.yScale = d3.scale.ordinal();
     that.barScale = d3.scale.ordinal();
 
-  /*  setting the domain for x-axis and y-axis */
-    that.yScale.domain(that.finalGraphData["Data"][0].data.map(function(d, i) {
+    /*  setting the domain for x-axis and y-axis */
+    that.yScale.domain(that.finalGraphData["Data"][0].data.map(function (d, i) {
         return d.label;
     })).rangeBands([that.height, that.categoryAxisStrokeWeight / 2], 0.2);
 
-    that.barScale.domain(that.finalGraphData["Data"].map(function(d, i) {
+    that.barScale.domain(that.finalGraphData["Data"].map(function (d, i) {
         return i;
     })).rangeRoundBands([0, that.yScale.rangeBand()]);
-    that.xScale.domain([that.valueAxisMinVal, that.valueAxisMaxVal]).range([0, that.width -that.padding.left -that.padding.right]);
+    that.xScale.domain([that.valueAxisMinVal, that.valueAxisMaxVal]).range([0, that.width - that.padding.left - that.padding.right]);
     /* Method call for drop labels Y-axis*/
 }
 
@@ -168,14 +168,14 @@ that.actualChartingAreaHeight = that.chartingAreaHeight;
  * @author    
  */
 
-BarChart.prototype.render = function(){
+BarChart.prototype.render = function () {
     var that = this;
-        that.chartingAreaStyle(that);
-        that.setActualChartingAreaStyle(that);
-        that.renderValueAxis(that);
-        that.renderLabelAxis(that);
-        that.renderBarSeries(that);
-    
+    that.chartingAreaStyle(that);
+    that.setActualChartingAreaStyle(that);
+    that.renderValueAxis(that);
+    that.renderLabelAxis(that);
+    that.renderBarSeries(that);
+
 }
 
 /*  -------------------------------------------------------------------------   */
@@ -188,14 +188,14 @@ BarChart.prototype.render = function(){
  * @author    
  */
 
-BarChart.prototype.chartingAreaStyle = function(that){
-   /*  styling the charting area/graph container  */
+BarChart.prototype.chartingAreaStyle = function (that) {
+    /*  styling the charting area/graph container  */
     d3.select('.chart').style({
         'background-color': that.chartingAreaBackgroundColor,
-        'height': that.chartingAreaHeight +"px",
+        'height': that.chartingAreaHeight + "px",
         'width': that.chartingAreaWidth + "px",
         'border': that.chartingAreaBorderThickness + "px solid " + that.chartingAreaBorderColor,
-            'left': that.chartingAreaXPosition + "px",
+        'left': that.chartingAreaXPosition + "px",
         'top': that.chartingAreaYPosition + "px",
         'overflow': 'hidden',
         'position': 'absolute'
@@ -212,23 +212,23 @@ BarChart.prototype.chartingAreaStyle = function(that){
  * @access    public
  * @author    
  */
-BarChart.prototype.setActualChartingAreaStyle = function(that){
+BarChart.prototype.setActualChartingAreaStyle = function (that) {
     that.actualChartArea = d3.select('.chart').append("div").classed("actualChartPane", true).style({
         'background-color': function () {
             return that.chartingAreaBackgroundColor;
         },
-       'height': that.chartingAreaHeight +"px",
+        'height': that.chartingAreaHeight + "px",
         'width': that.chartingAreaWidth + "px",
         'border': that.chartingAreaBorderThickness + "px solid " + that.chartingAreaBorderColor,
-})
+    })
 
     that.svg = that.actualChartArea.append("svg")
-        .attr("width", that.actualChartingAreaWidth)
-        .attr("height",that.actualChartingAreaHeight)
-        .style("padding-top",that.padding.top+'px')
-        .style("padding-right",that.padding.right +'px')
-        .style("padding-bottom",that.padding.bottom +'px')
-        .style("padding-left",that.padding.left + 'px');
+            .attr("width", that.actualChartingAreaWidth)
+            .attr("height", that.actualChartingAreaHeight)
+            .style("padding-top", that.padding.top + 'px')
+            .style("padding-right", that.padding.right + 'px')
+            .style("padding-bottom", that.padding.bottom + 'px')
+            .style("padding-left", that.padding.left + 'px');
 }
 
 
@@ -241,19 +241,21 @@ BarChart.prototype.setActualChartingAreaStyle = function(that){
  * @access    public
  * @author    
  */
-BarChart.prototype.renderValueAxis = function(that){
-    
+BarChart.prototype.renderValueAxis = function (that) {
+
     var valueAxis = d3.svg.axis()
-    .scale(that.xScale)
-    .orient("bottom");
-    
+            .scale(that.xScale)
+            .orient("bottom");
+            
+
     that.svg.append("g")
-    .attr("class", "value Axis")
-    .attr("transform", "translate(0," + (parseInt(that.height) - parseInt(that.valueAxisStrokeWeight) / 2) + ")")
-    .attr("fill","none")
-    .attr("stroke",that.valueAxisStrokeColor)
-    .attr("stroke-width", that.valueAxisStrokeWeight)
-    .call(valueAxis);
+            .attr("class", "valueAxis")
+            .attr("transform", "translate(0," + (parseInt(that.height) - parseInt(that.valueAxisStrokeWeight) / 2) + ")")
+            .attr("fill", "none")
+            .attr("stroke", that.valueAxisStrokeColor)
+            .attr("stroke-width", that.valueAxisStrokeWeight)
+            .style("color",that.valueAxisColor)
+            .call(valueAxis);
 }
 
 /*  -------------------------------------------------------------------------   */
@@ -265,16 +267,17 @@ BarChart.prototype.renderValueAxis = function(that){
  * @access    public
  * @author    
  */
-BarChart.prototype.renderLabelAxis = function(that){
+BarChart.prototype.renderLabelAxis = function (that) {
     var categoryAxis = d3.svg.axis()
             .scale(that.yScale)
             .orient("left")
-    
+
     that.svg.append("g")
-            .attr("class","labelAxis")
-            .attr("fill","none")
-            .attr("stroke",that.categoryAxisStrokeColor)
-            .attr("stroke-width",that.categoryAxisStrokeWidth)
+            .attr("class", "labelAxis")
+            .attr("fill", "none")
+            .attr("stroke", that.categoryAxisStrokeColor)
+            .attr("stroke-width", that.categoryAxisStrokeWidth)
+            .style("color",that.categoryAxisColor)
             .call(categoryAxis)
 }
 
---
+++ js/changeStyle.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    labelAxisStyle();
+
+});
+
+//Method to change the style of Label Axis//
+
+function labelAxisStyle() {
+    $(".font_size").change(function () {
+        $(".labelAxis text").css({'font-size': $(this).val() + 'px'});
+    });
+    $(".axisfont-color").change(function () {
+        $(".labelAxis text").css({'stroke': $(this).val() + ''});
+    });
+
+    $(".thickness").change(function () {
+        $(".labelAxis path").css({'stroke-width': $(this).val() + 'px'});
+    });
+    $(".axis-color").change(function () {
+        $(".labelAxis path").css({'stroke': $(this).val() + ''});
+    });
+    $('.titlefont-family').change(function () {
+        $('.labelAxis').css({'font-family': $(this).val() + ''});
+    });
+    valueAxisStyle();
+}
+;
+
+//Method to change the style of value Axis//
+
+function valueAxisStyle() {
+    $(".valuefont_size").change(function () {
+        $(".valueAxis text").css({'font-size': $(this).val() + 'px'});
+    });
+    $(".Font-color").change(function () {
+        $(".valueAxis text").css({'stroke': $(this).val() + ''});
+    });
+    $(".valueaxis-thickness").change(function () {
+        $(".valueAxis path").css({'stroke-width': $(this).val() + 'px'});
+    });
+    $(".valueaxis-color").change(function () {
+        $(".valueAxis path").css({'stroke': $(this).val() + ''});
+    });
+    $('.valueAxistitlefont-family').change(function () {
+        $('.valueAxis').css({'font-family': $(this).val() + ''});
+    });
+
+}
+;
